a91f1e76e4560bc1d07238677bb0049c21313d65
Florian Thiel <flth@flth.de>
2023-11-22 21:07:58 +0100

    initial

---
+++ mod.lua
@@ -0,0 +1,68 @@
+﻿function data()
+return {
+	info = {
+		minorVersion = 0,
+		severityAdd = "NONE",
+		severityRemove = "WARNING",
+		name = _("mod_name"),
+		description = _("mod_desc"),
+		authors = {
+		    {
+		        name = "ModWerkstatt",
+		        role = "CREATOR",
+		    },
+		},
+		tags = { "europe", "deutschland", "germany", "schweiz", "db", "waggon", "goods", "Intermodal", },
+		minGameVersion = 0,
+		dependencies = { },
+		url = { "" },
+
+		params = {
+			{
+				key = "sdgnss_t5_fake",
+				name = _("Fake_sdgnss_t5_wagen"),
+				values = { "No", "Yes", },
+				tooltip = _("option_fake_wagen_desc"),
+				defaultIndex = 0,
+			},
+        },
+	},
+	options = {
+	},
+
+	runFn = function (settings, modParams)
+	local params = modParams[getCurrentModId()]
+
+        local hidden = {
+			["hupac_fake.mdl"] = true,
+			["hupac2_fake.mdl"] = true,
+			["wascosa_fake.mdl"] = true,
+			["hupac_fake_walter.mdl"] = true,
+			["hupac_fake_wetron.mdl"] = true,
+			["hupac_fake_winner.mdl"] = true,
+			["hupac2_fake_walter.mdl"] = true,
+			["hupac2_fake_wetron.mdl"] = true,
+			["hupac2_fake_winner.mdl"] = true,
+			["wascosa_fake_walter.mdl"] = true,
+			["wascosa_fake_winner.mdl"] = true,
+			["wascosa_fake_wetron.mdl"] = true,
+        }
+
+		local modelFilter = function(fileName, data)
+			local modelName = fileName:match('/sdgnss_t5_([^/]*.mdl)')
+			return (modelName==nil or hidden[modelName]~=true)
+		end
+
+        if modParams[getCurrentModId()] ~= nil then
+			local params = modParams[getCurrentModId()]
+			if params["sdgnss_t5_fake"] == 0 then
+				addFileFilter("model/vehicle", modelFilter)
+			end
+		else
+			addFileFilter("model/vehicle", modelFilter)
+		end
+
+		addModifier( "loadModel", metadataHandler )
+	end
+	}
+end
---
+++ strings.lua
@@ -0,0 +1,122 @@
+function data()
+return {
+	en = {
+
+		["mod_name"] = "Sdgnss T5 Pocket cart",
+		["mod_desc"] = "In 2005, Hupac developed a new car based on the T4 with a lower footprint for mega trailers. Hupac purchased several hundred units of the T5, and Wascosa also purchased several units in 2009. The cars are designed for a maximum speed of 120 km/h when loaded.",
+
+		["option_fake_wagen_desc"] = "Should the fake versions of the Sdgnss T5 be available in the game?",
+		["Fake_sdgnss_t5_wagen"] = "Activate fake versions?",
+
+	    ["sdgnss_t5_menu"] = "Sdgnss T5 Pocket cart",
+		["sdgnss_t5_hupac_name"] = "Sdgnss T5 Hupac (2006)",
+		["sdgnss_t5_hupac2_name"] = "Sdgnss T5 Hupac/Wascosa (2006)",
+		["sdgnss_t5_hupac_name"] = "Sdgnss T5 Wascosa (2009)",
+        ["sdgnss_t5_desc"] = "In 2005, Hupac developed a new car based on the T4 with a lower footprint for mega trailers. Hupac purchased several hundred units of the T5, and Wascosa also purchased several units in 2009. The cars are designed for a maximum speed of 120 km/h when loaded.",
+
+		--- Artenreine Ladung ---
+		["sdgnss_t5_hupac_name_container"] = "Sdgnss T5 Hupac Container (2006)",
+		["sdgnss_t5_hupac_name_trailer"] = "Sdgnss T5 Hupac Trailer (2006)",
+		["sdgnss_t5_hupac2_name_container"] = "Sdgnss T5 Hupac/Wascosa Container (2006)",
+		["sdgnss_t5_hupac2_name_trailer"] = "Sdgnss T5 Hupac/Wascosa Trailer (2006)",
+		["sdgnss_t5_wascosa_name_container"] = "Sdgnss T5 Wascosa Container (2009)",
+		["sdgnss_t5_wascosa_name_trailer"] = "Sdgnss T5 Wascosa Trailer (2009)",
+
+		--- Ganzzug ---
+		["sdgnss_t5_hupac_name_winner"] = "Sdgnss T5 Hupac Winner (2006)",
+		["sdgnss_t5_hupac_name_walter"] = "Sdgnss T5 Hupac Walter (2006)",
+		["sdgnss_t5_hupac_name_wetron"] = "Sdgnss T5 Hupac Wetron (2006)",
+		["sdgnss_t5_hupac2_name_walter"] = "Sdgnss T5 Hupac/Wascosa Walter (2006)",
+		["sdgnss_t5_hupac2_name_wetron"] = "Sdgnss T5 Hupac/Wascosa Wetron (2006)",
+		["sdgnss_t5_hupac2_name_winner"] = "Sdgnss T5 Hupac/Wascosa Winner (2006)",
+		["sdgnss_t5_wascosa_name_walter"] = "Sdgnss T5 Wascosa Walter (2009)",
+		["sdgnss_t5_wascosa_name_wetron"] = "Sdgnss T5 Wascosa Wetron (2009)",
+		["sdgnss_t5_wascosa_name_winner"] = "Sdgnss T5 Wascosa Winner (2009)",
+
+		--- Fake ---
+        ["sdgnss_t5_menu_fake"] = "Sdgnss T5 Taschenwagen Fake",
+		["sdgnss_t5_hupac_name_fake"] = "Sdgnss T5 Hupac (2006) Fake",
+		["sdgnss_t5_hupac2_name_fake"] = "Sdgnss T5 Hupac/Wascosa (2006) Fake",
+		["sdgnss_t5_wascosa_name_fake"] = "Sdgnss T5 Wascosa (2009) Fake",
+
+		--- Fake Artenreine Ladung ---
+		["sdgnss_t5_hupac_name_fake_container"] = "Sdgnss T5 Hupac Container (2006) Fake",
+		["sdgnss_t5_hupac_name_fake_trailer"] = "Sdgnss T5 Hupac Trailer (2006) Fake",
+		["sdgnss_t5_hupac2_name_fake_container"] = "Sdgnss T5 Hupac/Wascosa Container (2006) Fake",
+		["sdgnss_t5_hupac2_name_fake_trailer"] = "Sdgnss T5 Hupac/Wascosa Trailer (2006) Fake",
+		["sdgnss_t5_wascosa_name_fake_container"] = "Sdgnss T5 Wascosa Container (2009) Fake",
+		["sdgnss_t5_wascosa_name_fake_trailer"] = "Sdgnss T5 Wascosa Trailer (2009) Fake",
+
+		--- Fake Ganzzug ---
+		["sdgnss_t5_hupac_name_fake_winner"] = "Sdgnss T5 Hupac Winner (2006) Fake",
+		["sdgnss_t5_hupac_name_fake_walter"] = "Sdgnss T5 Hupac Walter (2006) Fake",
+		["sdgnss_t5_hupac_name_fake_wetron"] = "Sdgnss T5 Hupac Wetron (2006) Fake",
+		["sdgnss_t5_hupac2_name_fake_walter"] = "Sdgnss T5 Hupac/Wascosa Walter (2006) Fake",
+		["sdgnss_t5_hupac2_name_fake_wetron"] = "Sdgnss T5 Hupac/Wascosa Wetron (2006) Fake",
+		["sdgnss_t5_hupac2_name_fake_winner"] = "Sdgnss T5 Hupac/Wascosa Winner (2006) Fake",
+		["sdgnss_t5_wascosa_name_fake_walter"] = "Sdgnss T5 Wascosa Walter (2009) Fake",
+		["sdgnss_t5_wascosa_name_fake_wetron"] = "Sdgnss T5 Wascosa Wetron (2009) Fake",
+		["sdgnss_t5_wascosa_name_fake_winner"] = "Sdgnss T5 Wascosa Winner (2009) Fake",
+
+	},
+	de = {
+
+		["mod_name"] = "Sdgnss T5 Taschenwagen",
+		["mod_desc"] = "2005 lies die Hupac einen neuen Wagen auf der Basis des T4 entwickeln mit einer niedrigeren Aufstandshöfe für Megatrailer. Hupac beschaffte mehrere Hundert Einheiten des T5, 2009 beschaffte auch die Wascosa mehrere Einheiten. Die Wagen sind für eine Höchgeschwindigkeit von 120 km/h im beladenen Zustand ausgelegt.",
+
+		["option_fake_wagen_desc"] = "Sollen die Fake-Versionen des Sdgnss T5 verfügbar sein im Spiel?",
+		["Fake_sdgnss_t5_wagen"] = "Fake-Versionen aktivieren?",
+
+        ["sdgnss_t5_menu"] = "Sdgnss T5 Taschenwagen",
+		["sdgnss_t5_hupac_name"] = "Sdgnss T5 Hupac (2006)",
+		["sdgnss_t5_hupac2_name"] = "Sdgnss T5 Hupac/Wascosa (2006)",
+		["sdgnss_t5_wascosa_name"] = "Sdgnss T5 Wascosa (2009)",
+        ["sdgnss_t5_desc"] = "2005 lies die Hupac einen neuen Wagen auf der Basis des T4 entwickeln mit einer niedrigeren Aufstandshöfe für Megatrailer. Hupac beschaffte mehrere Hundert Einheiten des T5, 2009 beschaffte auch die Wascosa mehrere Einheiten. Die Wagen sind für eine Höchgeschwindigkeit von 120 km/h im beladenen Zustand ausgelegt.",
+
+		--- Artenreine Ladung ---
+		["sdgnss_t5_hupac_name_container"] = "Sdgnss T5 Hupac Container (2006)",
+		["sdgnss_t5_hupac_name_trailer"] = "Sdgnss T5 Hupac Trailer (2006)",
+		["sdgnss_t5_hupac2_name_container"] = "Sdgnss T5 Hupac/Wascosa Container (2006)",
+		["sdgnss_t5_hupac2_name_trailer"] = "Sdgnss T5 Hupac/Wascosa Trailer (2006)",
+		["sdgnss_t5_wascosa_name_container"] = "Sdgnss T5 Wascosa Container (2009)",
+		["sdgnss_t5_wascosa_name_trailer"] = "Sdgnss T5 Wascosa Trailer (2009)",
+
+		--- Ganzzug ---
+		["sdgnss_t5_hupac_name_winner"] = "Sdgnss T5 Hupac Winner (2006)",
+		["sdgnss_t5_hupac_name_walter"] = "Sdgnss T5 Hupac Walter (2006)",
+		["sdgnss_t5_hupac_name_wetron"] = "Sdgnss T5 Hupac Wetron (2006)",
+		["sdgnss_t5_hupac2_name_walter"] = "Sdgnss T5 Hupac/Wascosa Walter (2006)",
+		["sdgnss_t5_hupac2_name_wetron"] = "Sdgnss T5 Hupac/Wascosa Wetron (2006)",
+		["sdgnss_t5_hupac2_name_winner"] = "Sdgnss T5 Hupac/Wascosa Winner (2006)",
+		["sdgnss_t5_wascosa_name_walter"] = "Sdgnss T5 Wascosa Walter (2009)",
+		["sdgnss_t5_wascosa_name_wetron"] = "Sdgnss T5 Wascosa Wetron (2009)",
+		["sdgnss_t5_wascosa_name_winner"] = "Sdgnss T5 Wascosa Winner (2009)",
+
+		--- Fake ---
+        ["sdgnss_t5_menu_fake"] = "Sdgnss T5 Taschenwagen Fake",
+		["sdgnss_t5_hupac_name_fake"] = "Sdgnss T5 Hupac (2006) Fake",
+		["sdgnss_t5_hupac2_name_fake"] = "Sdgnss T5 Hupac/Wascosa (2006) Fake",
+		["sdgnss_t5_wascosa_name_fake"] = "Sdgnss T5 Wascosa (2009) Fake",
+
+		--- Fake Artenreine Ladung ---
+		["sdgnss_t5_hupac_name_fake_container"] = "Sdgnss T5 Hupac Container (2006) Fake",
+		["sdgnss_t5_hupac_name_fake_trailer"] = "Sdgnss T5 Hupac Trailer (2006) Fake",
+		["sdgnss_t5_hupac2_name_fake_container"] = "Sdgnss T5 Hupac/Wascosa Container (2006) Fake",
+		["sdgnss_t5_hupac2_name_fake_trailer"] = "Sdgnss T5 Hupac/Wascosa Trailer (2006) Fake",
+		["sdgnss_t5_wascosa_name_fake_container"] = "Sdgnss T5 Wascosa Container (2009) Fake",
+		["sdgnss_t5_wascosa_name_fake_trailer"] = "Sdgnss T5 Wascosa Trailer (2009) Fake",
+
+		--- Fake Ganzzug ---
+		["sdgnss_t5_hupac_name_fake_winner"] = "Sdgnss T5 Hupac Winner (2006) Fake",
+		["sdgnss_t5_hupac_name_fake_walter"] = "Sdgnss T5 Hupac Walter (2006) Fake",
+		["sdgnss_t5_hupac_name_fake_wetron"] = "Sdgnss T5 Hupac Wetron (2006) Fake",
+		["sdgnss_t5_hupac2_name_fake_walter"] = "Sdgnss T5 Hupac/Wascosa Walter (2006) Fake",
+		["sdgnss_t5_hupac2_name_fake_wetron"] = "Sdgnss T5 Hupac/Wascosa Wetron (2006) Fake",
+		["sdgnss_t5_hupac2_name_fake_winner"] = "Sdgnss T5 Hupac/Wascosa Winner (2006) Fake",
+		["sdgnss_t5_wascosa_name_fake_walter"] = "Sdgnss T5 Wascosa Walter (2009) Fake",
+		["sdgnss_t5_wascosa_name_fake_wetron"] = "Sdgnss T5 Wascosa Wetron (2009) Fake",
+		["sdgnss_t5_wascosa_name_fake_winner"] = "Sdgnss T5 Wascosa Winner (2009) Fake",
+
+	},
+}
+end
